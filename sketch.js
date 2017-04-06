61cb130d1549164e2e403814d889fac5c5a43055
Lisa Evelyn <LisaEvelyn@users.noreply.github.com>
2017-04-06 12:12:38 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,14 +11,14 @@ function setup() {
 
 function draw() {
 	stroke(r, g, b, 5)
-	fill(r, g, b, 40);
-	randomX = random(-20,20);
-	randomY = random(-20,20);
-  	ellipse(mouseX+randomX, mouseY+randomY, 10, 10);
+	fill(r, g, b, 50);
+	randomX = random(-50,50);
+	randomY = random(-50,50);
+  	ellipse(mouseX+randomX, mouseY+randomY, 15, 15);
 	colorUpdate();
 	randomX = random(-20,20);
 	randomY = random(-20,20);
-  	ellipse(mouseX+randomX, mouseY+randomY, 10, 10);
+  	ellipse(mouseX+randomX, mouseY+randomY, 5, 5);
 	colorUpdate();
 }
 
