8fb5cc52b25148c87f4901393dc404a3613342cf
Lisa Evelyn <LisaEvelyn@users.noreply.github.com>
2021-02-19 16:31:52 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,16 +10,18 @@ function setup() {
 }
 
 function draw() {
-	stroke(r, g, b, 5)
-	fill(r, g, b, 50);
-	randomX = random(-50,50);
-	randomY = random(-50,50);
-  	ellipse(mouseX+randomX, mouseY+randomY, 15, 15);
-	colorUpdate();
-	randomX = random(-20,20);
-	randomY = random(-20,20);
-  	ellipse(mouseX+randomX, mouseY+randomY, 5, 5);
-	colorUpdate();
+	if (mouseX != pmouseX && mouseY != pmouseY){
+		stroke(r, g, b, 5)
+		fill(r, g, b, 50);
+		randomX = random(-50,50);
+		randomY = random(-50,50);
+  		ellipse(mouseX+randomX, mouseY+randomY, 15, 15);
+		colorUpdate();
+		randomX = random(-20,20);
+		randomY = random(-20,20);
+  		ellipse(mouseX+randomX, mouseY+randomY, 5, 5);
+		colorUpdate();
+	}
 }
 
 function windowResized () {
